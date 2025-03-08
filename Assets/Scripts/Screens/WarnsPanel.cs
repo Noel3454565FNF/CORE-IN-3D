3e048023f6268e8101d99aa95c0a5bdad88fd052
Noel345 <58745415+Noel3454565FNF@users.noreply.github.com>
2025-03-08 20:14:03 +0100

    things

---
+++ Assets/Scripts/Screens/WarnsPanel.cs
@@ -0,0 +1,88 @@
+using JetBrains.Annotations;
+using System.Collections;
+using UnityEngine;
+
+
+
+public class WarnsPanel : MonoBehaviour
+{
+    [Header("Screen Components")]
+    public bool test = true;
+
+
+
+
+
+
+
+    public void CoreOverheatWarnON()
+    {
+
+    }
+    public void CoreOverheatWarnOFF()
+    {
+
+    }
+    public void CoreCritOverheatWarnON()
+    {
+
+    }
+    public void CoreCritOverheatWarnOFF()
+    {
+
+    }
+    public void CoreInstabilityWarnON()
+    {
+
+    }
+    public void CoreInstabilityWarnOFF()
+    {
+
+    }
+    public void CoreFaultWarnON()
+    {
+
+    }
+    public void CoreFaultWarnOFF()
+    {
+
+    }
+    
+
+
+    public void LasersDamageWarnON()
+    {
+
+    }
+    public void LasersDamageWarnOFF()
+    {
+
+    }
+    public void LasersSurgeWarnON()
+    {
+
+    }
+    public void LasersSurgeWarnOFF()
+    {
+
+    }
+    public void LasersPurgeWarnON()
+    {
+
+    }
+    public void LasersPurgeWarnOFF()
+    {
+
+    }
+    
+
+
+    public void ShieldOverloadWarnON()
+    {
+
+    }
+    public void ShieldOverloadWarnOFF()
+    {
+
+    }
+}
